89519d881154a68d6eee099fb78e723124ae573d
Ahmed <Wegostationdev@gmail.com>
2025-05-18 10:33:48 +0200

    commit

--- resources/views/Admin/Live/Live.blade.php
+++ resources/views/Admin/Live/Live.blade.php
@@ -647,7 +647,21 @@ return 'admin';
                                                     </label>
                                                     <div class="select2-danger" data-select2-id="33">
                                                         <div class="position-relative" data-select2-id="443">
-                                                            <select id="select2Danger" name="user_id[]" class="select2 form-select" multiple>
+                                                            <select id="select2Danger" name="user_id[]"
+                                                                class="select2 form-select select2-hidden-accessible"
+                                                                multiple=""
+                                                                data-select2-id="select2Danger{{ $session->id }}"
+                                                                tabindex="-1" aria-hidden="true">
+                                                                {{-- @foreach ($session->users as $user)
+                                                                <option value="{{ $user->id }}" selected
+                                                                    data-select2-id="edit{{ $session->id }}{{ $user->id }}">
+                                                                    {{ $user->nick_name }}</option>
+                                                                @endforeach
+                                                                @foreach ($users as $user)
+                                                                <option value="{{ $user->id }}"
+                                                                    data-select2-id="{{ $user->id }}">
+                                                                    {{ $user->nick_name }}</option>
+                                                                @endforeach --}}
                                                                 @foreach ($session->users as $user)
                                                                     <option value="{{ $user->id }}" selected>{{ $user->nick_name }}</option>
                                                                 @endforeach
@@ -656,7 +670,6 @@ return 'admin';
                                                                     <option value="{{ $user->id }}">{{ $user->nick_name }}</option>
                                                                 @endforeach
                                                             </select>
-
                                                         </div>
                                                     </div>
                                                 </div>
