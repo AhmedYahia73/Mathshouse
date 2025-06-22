cf979be242bdb99ddd31c3c59ed28610d75e4687
basantsaed2 <basantsaed718@gmail.com>
2025-06-22 15:56:47 +0300

    edit mistakepdf ,questions_answer ,quize

--- resources/views/Quiz.blade.php
+++ resources/views/Quiz.blade.php
@@ -28,7 +28,6 @@
             padding: 1rem 0;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
             z-index: 1000;
-            text-align: center;
         }
 
         .header-logo img {
