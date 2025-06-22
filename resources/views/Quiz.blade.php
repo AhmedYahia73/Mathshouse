538ae3a115e4ff619fb90b89d3d4235884f974f0
basantsaed2 <basantsaed718@gmail.com>
2025-06-22 16:38:23 +0300

    commit

--- resources/views/Quiz.blade.php
+++ resources/views/Quiz.blade.php
@@ -139,8 +139,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Report</title>
-    <!-- Add Font Awesome for icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <style>
         * {
             margin: 0;
