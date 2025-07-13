d61abeb7117b78095f67e6ddf6d2c5ddb34d27f8
Ahmed <Wegostationdev@gmail.com>
2025-07-13 13:00:52 +0200

    commit

--- app/Http/Controllers/Api/Admin/Live/SessionController.php
+++ app/Http/Controllers/Api/Admin/Live/SessionController.php
@@ -122,10 +122,57 @@ class SessionController extends Controller
     }
 
     public function modify(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'date' => ['nullable', 'date'], 
+            'name' => ['nullable'],
+            'link' => ['nullable'], 
+            'material_link' => ['nullable'],
+            'from' => ['nullable', 'regex:/^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/'], 
+            'to' => ['nullable', 'regex:/^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/'], 
+            'lesson_id' => ['nullable', 'exists:lessons,id'], 
+            'course_id' => ['exists:courses,id'], 
+            'teacher_id' => ['nullable', 'exists:users,id'], 
+            'group_id' => ['exists:session_groups,id'],
+            'type' => ['nullable', 'in:group,private,session'],  
+            'session_types' => ['nullable', 'in:explanation,re_explanation,mistakes'],
+            'teacher_material'  => ['sometimes']
+        ]);
+        if ($validator->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validator->errors(),
+            ],400);
+        }
         
+        $session = $this->session
+        ->where('id', $id)
+        ->first();
+        $session->date = $request->date ?? $session->date;
+        $session->name = $request->name ?? $session->name;
+        $session->link = $request->link ?? $session->link;
+        $session->material_link = $request->material_link ?? $session->material_link;
+        $session->from = $request->from ?? $session->from;
+        $session->to = $request->to ?? $session->to;
+        $session->lesson_id = $request->lesson_id ?? $session->lesson_id;
+        $session->course_id = $request->course_id ?? $session->course_id;
+        $session->teacher_id = $request->teacher_id ?? $session->teacher_id;
+        $session->group_id = $request->group_id ?? $session->group_id;
+        $session->type = $request->type ?? $session->type;
+        $session->session_types = $request->session_types ?? $session->session_types;
+        $session->teacher_material = $request->teacher_material ?? $session->teacher_material;
+        $session->save();
+
+        return response()->json([
+            'success' => 'You update session success'
+        ]);
     }
 
-    public function delete(Request $request, $id){
-        
+    public function delete(Request $request, $id){ 
+        $session = $this->session
+        ->where('id', $id)
+        ->delete();
+
+        return response()->json([
+            'success' => 'You delete session success'
+        ]);
     }
 }
