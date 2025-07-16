cee8e820d15ebe3e99015a3362c4065a433bf654
Ahmed <Wegostationdev@gmail.com>
2025-07-16 12:03:15 +0200

    end academic

--- app/Http/Controllers/Api/Admin/Live/AcademicController.php
+++ app/Http/Controllers/Api/Admin/Live/AcademicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Admin\Live;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Category;
 use App\Models\Course;
@@ -51,6 +52,78 @@ class AcademicController extends Controller
     }
 
     public function academic(Request $request){
+        $validator = Validator::make($request->all(), [
+            'category_id'  => ['required', 'exists:categories,id'],
+            'course_id'  => ['required', 'exists:courses,id'],
+            'chapter_id'  => ['exists:chapters,id'],
+            'lesson_id'  => ['exists:lessons,id'],
+            'group_ids'  => ['array'],
+            'group_ids.*'  => ['exists:session_groups,id'], 
+            'students_ids'  => ['array'],
+            'students_ids.*'  => ['exists:users,id'], 
+        ]);
+        if ($validator->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validator->errors(),
+            ],400);
+        }
 
+        $lessons = collect([]);
+        if($request->lesson_id){
+            $lessons = $this->lessons
+            ->select('id', 'lesson_name')
+            ->where('id', $request->lesson_id)
+            ->get();
+        }
+        elseif($request->chapter_id){
+            $lessons = $this->lessons
+            ->select('id', 'lesson_name')
+            ->where('chapter_id', $request->chapter_id)
+            ->get();
+        }
+        elseif($request->course_id){
+            $lessons = $this->lessons
+            ->select('id', 'lesson_name')
+            ->whereHas('chapter', function($query) use($request){
+                $query->where('course_id', $request->course_id);
+            })
+            ->get();
+        }
+
+        $students = collect([]);
+        if ($request->students_ids && count($request->students_ids) > 0) {
+            $users = $this->students
+            ->whereIn('id', $request->students_ids)
+            ->get();
+            $students = $students->merge($users);
+        }
+        if ($request->group_ids && count($request->group_ids) > 0) {
+            $users = $this->groups
+            ->whereIn('id', $request->group_ids)
+            ->with('students')
+            ->get()
+            ?->pluck('students')
+            ->flatten(1);
+            $students = $students->merge($users);
+        }
+        $users = $this->students;
+
+        $students = $students
+        ->map(function($item) use($lessons, $users){
+            $item->lessons = clone $lessons->map(function($element) use($item, $users){
+                $element = clone $element;
+                $element->attendance = !empty($users
+                ->where('id', $item->id)
+                ->first()
+                ?->attendance($element->id)?->first());
+                return $element;
+            });
+            return $item;
+        });
+
+        return response()->json([
+            'lessons' => $lessons,
+            'students' => $students,
+        ]);
     }
 }
