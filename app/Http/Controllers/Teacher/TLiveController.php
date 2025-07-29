00eb819b4e14ad1b4f02b85c8be7150e3b2deae7
Ahmed <Wegostationdev@gmail.com>
2025-07-29 12:53:20 +0200

    commit

--- app/Http/Controllers/Teacher/TLiveController.php
+++ app/Http/Controllers/Teacher/TLiveController.php
@@ -20,31 +20,25 @@ class TLiveController extends Controller
     }
 
     public function upload_teacher_material(Request $request){
-        $img_name = null;
+        $file_name = null;
         extract($_FILES['ans_teacher_material']);
         if( !empty($name) ){
-            $extension_arr = ['png', 'jpg', 'jpeg', 'svg', 'webp'];
-            $extension = explode('.', $name);
-            $extension = end($extension);
-            $extension = strtolower($extension);
-            if ( in_array($extension, $extension_arr) ) {
-                $img_name = rand(0, 1000) . now() . $name;
-                $img_name = str_replace([' ', ':', '-'], 'X', $img_name);
-                $img_name = 'files/teacher_pdf/' . $img_name;
-            } 
+            $file_name = rand(0, 1000) . now() . $name;
+            $file_name = str_replace([' ', ':', '-'], 'X', $file_name);
+            $file_name = 'files/teacher_pdf/' . $file_name;
         }
         $session = Session::where('id', $request->session_id)
         ->first();
         $this->delete_image_path($session->ans_teacher_material ?? '');
         $session->update([
-            'ans_teacher_material' => $img_name,
+            'ans_teacher_material' => $file_name,
         ]);
         $folder = 'files/teacher_pdf/';
 
         if (!is_dir($folder)) {
             mkdir($folder, 0755, true);
         }
-        move_uploaded_file($tmp_name, $img_name);
+        move_uploaded_file($tmp_name, $file_name);
 
         return redirect()->back();
     }
