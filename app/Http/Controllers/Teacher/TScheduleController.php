a6defab4770cd66a8ce7648659a76dfe1830c15c
Ahmed <Wegostationdev@gmail.com>
2025-08-03 15:45:21 +0200

    commit

--- app/Http/Controllers/Teacher/TScheduleController.php
+++ app/Http/Controllers/Teacher/TScheduleController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class TScheduleController extends Controller
 {
     public function view(Request $request){
-        return view('Teacher.Schedule.Schedule');
+        $page_name = 'Schedule'; 
+        return view('Teacher.Schedule.Schedule', compact('page_name'));
     }
 }
