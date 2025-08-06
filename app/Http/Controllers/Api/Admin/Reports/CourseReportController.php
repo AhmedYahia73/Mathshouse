e13065227ecb98e292a88990b2cc8bbe4582c337
Ahmed <Wegostationdev@gmail.com>
2025-08-06 10:26:30 +0200

    commit

--- app/Http/Controllers/Api/Admin/Reports/CourseReportController.php
+++ app/Http/Controllers/Api/Admin/Reports/CourseReportController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Course;
 
 class CourseReportController extends Controller
 {
