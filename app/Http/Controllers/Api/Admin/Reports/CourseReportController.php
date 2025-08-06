14236bc04bf15ede711b0bf0ca30bef3618eb2e2
Ahmed <Wegostationdev@gmail.com>
2025-08-06 10:20:23 +0200

    commit

--- app/Http/Controllers/Api/Admin/Reports/CourseReportController.php
+++ app/Http/Controllers/Api/Admin/Reports/CourseReportController.php
@@ -14,66 +14,122 @@ class CourseReportController extends Controller
 
     public function view(Request $request){
         $users = $this->users
-    ->with([
-        'payment_req_approve' => function ($query) {
-            $query->where('module', 'Chapters')
-                ->with(['chapters_order.chapter.course']);
-        }
-    ])
-    ->whereHas('payment_req_approve.chapters_order.chapter.course')
-    ->get()
-    ->map(function ($user) {
-        return [
-            'id' => $user->id,
-            'f_name' => $user->f_name,
-            'l_name' => $user->l_name,
-            'nick_name' => $user->nick_name,
-            'courses' => $user->payment_req_approve
-                ->flatMap(function ($payment) {
-                    return $payment->chapters_order->map(function ($order) {
-                        if ($order->chapter && $order->chapter->course) {
-                            return [
-                                'course_name' => $order->chapter->course->course_name
-                            ];
-                        }
-                        return null;
-                    })->filter(); // remove nulls
-                })->values(),
-        ];
-    });
+        ->with([
+            'payment_req_approve' => function ($query) {
+                $query->where('module', 'Chapters')
+                    ->with(['chapters_order.chapter.course']);
+            }
+        ])
+        ->whereHas('payment_req_approve.chapters_order.chapter.course')
+        ->get()
+        ->map(function ($user) {
+            return [
+                'id' => $user->id,
+                'f_name' => $user->f_name,
+                'l_name' => $user->l_name,
+                'nick_name' => $user->nick_name,
+                'courses' => $user->payment_req_approve
+                    ->flatMap(function ($payment) {
+                        return $payment->chapters_order->map(function ($order) {
+                            if ($order->chapter && $order->chapter->course) {
+                                return [
+                                    'course_name' => $order->chapter->course->course_name
+                                ];
+                            }
+                            return null;
+                        })->filter(); // remove nulls
+                    })->values(),
+            ];
+        });
 
-   
-        // $categories = Category::all();
-        // $courses = Course::all();
+        $categories = Category::all();
+        $courses = Course::all();
 
         return response()->json([
-            'users' => $users
+            'users' => $users,
+            'categories' => $categories,
+            'courses' => $courses,
         ]);
     }
 
     public function filter(Request $request){
         $validator = Validator::make($request->all(), [
-            'grade' => ['numeric', 'min:1', 'max:13'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'course_id' => ['exists:courses,id'],
         ]);
         if ($validator->fails()) { 
             return response()->json([
                 'errors' => $validator->errors(),
             ],400);
         }
-
-        $users = $this->users
-        ->where('position', 'student')
-        ->where('grade', $request->grade)
-        ->get()
-        ->map(function($item){
-            return [
-                'id' => $item->id,
-                'email' => $item->email,
-                'phone' => $item->phone,
-                'nick_name' => $item->nick_name,
-                'grade' => 'Grade ' . $item->grade,
-            ];
-        });
+        if($request->course_id){
+            $users = $this->users
+            ->with([
+                'payment_req_approve' => function ($query) use($request){
+                    $query->where('module', 'Chapters')
+                        ->with(['chapters_order.chapter.course' => function($query){
+                            $query->where('id', $request->course_id);
+                        }]);
+                }
+            ])
+            ->whereHas('payment_req_approve.chapters_order.chapter.course', function($query){
+                $query->where('id', $request->course_id);
+            })
+            ->get()
+            ->map(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'f_name' => $user->f_name,
+                    'l_name' => $user->l_name,
+                    'nick_name' => $user->nick_name,
+                    'courses' => $user->payment_req_approve
+                        ->flatMap(function ($payment) {
+                            return $payment->chapters_order->map(function ($order) {
+                                if ($order->chapter && $order->chapter->course) {
+                                    return [
+                                        'course_name' => $order->chapter->course->course_name
+                                    ];
+                                }
+                                return null;
+                            })->filter(); // remove nulls
+                        })->values(),
+                ];
+            });
+        }
+        else{
+            $users = $this->users
+            ->with([
+                'payment_req_approve' => function ($query) use($request){
+                    $query->where('module', 'Chapters')
+                        ->with(['chapters_order.chapter.course' => function($query){
+                            $query->where('category_id', $request->category_id);
+                        }]);
+                }
+            ])
+            ->whereHas('payment_req_approve.chapters_order.chapter.course', function($query){
+                $query->where('category_id', $request->category_id);
+            })
+            ->get()
+            ->map(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'f_name' => $user->f_name,
+                    'l_name' => $user->l_name,
+                    'nick_name' => $user->nick_name,
+                    'courses' => $user->payment_req_approve
+                        ->flatMap(function ($payment) {
+                            return $payment->chapters_order->map(function ($order) {
+                                if ($order->chapter && $order->chapter->course) {
+                                    return [
+                                        'course_name' => $order->chapter->course->course_name
+                                    ];
+                                }
+                                return null;
+                            })->filter(); // remove nulls
+                        })->values(),
+                ];
+            });
+        }
 
         return response()->json([
             'users' => $users
