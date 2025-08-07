ba488d1d152bc1d3a5c7ea1b52db14acbf9c6299
Ahmed <Wegostationdev@gmail.com>
2025-08-07 15:15:21 +0200

    commit

--- routes/admin.php
+++ routes/admin.php
@@ -196,5 +196,6 @@ Route::middleware(['auth:sanctum', 'auth.MobileAdmin'])->group(function(){
         Route::get('/quiz_list/{user_id}', 'quiz_list');
         Route::get('/quiz_mistakes/{id}', 'quiz_mistakes');
         Route::get('/quiz_report/{id}', 'quiz_report');
+        Route::post('/generatePdf', 'generatePdf');
     });
 });
