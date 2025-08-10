7c7c5a007a8756d6fd0b7dd27472df9c41a3da47
Ahmed <Wegostationdev@gmail.com>
2025-08-10 09:23:22 +0200

    commit

--- app/Http/Controllers/Api/User/Exam/QuestionReportController.php
+++ app/Http/Controllers/Api/User/Exam/QuestionReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\User\Exam;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\ReportQuestion;
 
