456a320fe17e5b9161c8c727f36f0c92bcc23e3f
Ahmed <Wegostationdev@gmail.com>
2025-08-10 14:39:31 +0200

    commit

--- app/Http/Controllers/Api/User/Profile/ProfileController.php
+++ app/Http/Controllers/Api/User/Profile/ProfileController.php
@@ -26,17 +26,18 @@ class ProfileController extends Controller
             'parent_phone' => $user->parent_phone,
             'parent_email' => $user->parent_email,
             'grade' => $user->grade,
-            'image_link' => $user->image_link,
+            'image' => $user->image_link,
         ]);
     }
 
     public function update_profile(Request $request){
         $validator = Validator::make($request->all(), [
             'nick_name' => 'unique:users,nick_name,' . $request->user()->id,
-            'email' => 'unique:users,email,' . $request->user()->id,
+            'email' => 'email|unique:users,email,' . $request->user()->id,
             'phone' => 'unique:users,phone,' . $request->user()->id,
             'parent_phone' => 'unique:users,parent_phone,' . $request->user()->id,
-            'parent_email' => 'unique:users,parent_email,' . $request->user()->id,
+            'parent_email' => 'email|unique:users,parent_email,' . $request->user()->id,
+            'extra_email' => 'email|unique:users,extra_email,' . $request->user()->id,
         ]);
         if ($validator->fails()) { // if Validate Make Error Return Message Error
             return response()->json([
@@ -53,9 +54,13 @@ class ProfileController extends Controller
         $user->parent_phone = $request->parent_phone ?? $user->parent_phone;
         $user->parent_email = $request->parent_email ?? $user->parent_email;
         $user->grade = $request->grade ?? $user->grade;
+        $user->extra_email = $request->extra_email ?? $user->extra_email;
         if($request->image){
             $image_path = $this->store_base64($request->image, 'images/users');
             $user->image = $image_path;
+        }
+        if($request->password){
+            $user->password = bcrypt($request->password);
         }
         $user->save();
 
