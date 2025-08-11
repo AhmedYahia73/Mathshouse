94987bf0d4d61737cb46524f50b959636afbfbff
Ahmed <Wegostationdev@gmail.com>
2025-08-11 09:22:52 +0200

    commit

--- routes/user.php
+++ routes/user.php
@@ -145,5 +145,6 @@ Route::middleware(['auth:sanctum', 'auth.MobileUser'])->group(function(){
     Route::controller(CourseController::class)->prefix('courses')
     ->group(function(){
         Route::get('/', 'lists');
+        Route::get('/chaters_data/{id}', 'chaters_data');
     });
 });
