c7ca6b1e7340a8c364f5efafe8226aedc578b5ea
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-09-22 21:42:42 +0200

    commit

--- app/Http/Controllers/Api/User/Courses/CourseController.php
+++ app/Http/Controllers/Api/User/Courses/CourseController.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\PaymentEmail;
 use Illuminate\Support\Facades\Cookie;
 
+use App\service\Fawry\FawryPayService;
+use App\service\Fawry\PlaceOrderFawry;
+
 use App\Models\Category;
 use App\Models\Course;
 use App\Models\Chapter;
@@ -28,6 +31,7 @@ use App\Models\PaymentOrder;
 
 class CourseController extends Controller
 {
+    protected $fawryPayService;
     public function __construct(private Category $categories,
     private Lesson $lessons, private Question $questions,
     private quizze $quiz, private IdeaLesson $idea,
@@ -36,9 +40,14 @@ class CourseController extends Controller
     private PromoCourse $promo_course,
     private PaymentMethod $payment_method,
     private Course $course,
-    private Chapter $chapters){}
+    private Chapter $chapters,
+    FawryPayService $fawryPayService){
+        $this->fawryPayService = $fawryPayService;
+    }
     use Image;
     use PaymentPaymob;
+    use FawryPayService;
+    use PlaceOrderFawry;
 
     public function lists(Request $request){
         
@@ -450,4 +459,115 @@ class CourseController extends Controller
             'chapters' => $chapters,
         ]);
     }
+    
+    use PlaceOrder; // This Trait For Make Order 
+    public function payAtFawry(Request $request)
+    {
+        $request['customerProfileId'] = $request->user()->id ;
+        $request['customerMobile'] = $request->user()->phone ;
+        // Make Random Number For MerchantRefNumber
+        do {
+            $length = 6;
+            // Generate a random number of the desired length
+            $min = pow(10, $length - 1); // Minimum number based on length ( 1000000000 for 10 digits)
+            $max = pow(10, $length) - 1; // Maximum number based on length ( 9999999999 for 10 digits)
+            $randomNumber = random_int($min, $max);
+
+            // Check if this number already exists in Payment
+            $exists = $this->wallet::where('merchantRefNum', $randomNumber)->exists();
+            $request['merchantRefNum'] = $randomNumber ;
+          } while ($exists); // Repeat until a unique number is generated
+                // End Random Number For MerchantRefNumber
+
+        // Validate incoming request data
+        $request->validate([
+            // 'customerName' => 'required|string',
+            // 'customerMobile' => 'required|string',
+            // 'customerEmail' => 'required|email',
+            // 'customerProfileId' => 'required|numeric',
+            // 'merchantRefNum' => 'required|string',
+            'amount' => 'required|numeric',
+            // 'description' => 'required|string',
+            'chargeItems' => 'required|array',
+            'chargeItems.*.price' => 'required|numeric',
+            'chargeItems.*.chapters_id' => 'required|exists:chapters,id',
+        ]);
+      
+        
+         // Start Create Order If Operation Payment Success
+        $placeOrder = $this->placeOrder($request);
+         // Start Create Order If Operation Payment Success
+     
+        // Extract data
+        $merchantRefNum = $request->merchantRefNum;
+        $customerProfileId =$request->customerProfileId;
+        $paymentMethod = 'PayAtFawry';
+        $amount = $placeOrder['chargeItems']['price'];
+
+        // Generate signature
+        $signature = $this->fawryPayService->generateSignature($merchantRefNum, $customerProfileId, $paymentMethod, $amount);
+        // Prepare the request payload
+        $data = [
+            'merchantCode' => env('FAWRY_MERCHANT_CODE'),
+            'customerName' => $request->customerName,
+            'customerMobile' => $request->customerMobile,
+            'customerEmail' => $request->customerEmail,
+            'customerProfileId' => $request->customerProfileId, // old Request => $request->customerProfileId Changed Cause When Pass Token I Will Get user_id
+            'merchantRefNum' => $merchantRefNum,
+            'amount' => number_format($placeOrder['chargeItems']['price'], 2, '.', ''),
+            'paymentExpiry' => $request->paymentExpiry ?? null,
+            'currencyCode' => 'EGP',
+            'language' => $request->language ?? 'en-gb',
+            'chargeItems' => [$placeOrder['chargeItems']],
+            'signature' => $signature,
+            'paymentMethod' => $paymentMethod,
+            'description' => $request->description
+        ];
+ 
+        
+            
+        // Make the charge request
+       $response = $this->fawryPayService->createCharge($data);
+        // Return response to the client
+
+   
+        
+        return response()->json($response);
+    }
+
+    public function checkPaymentStatus(Request $request)
+    {
+        // Validate incoming request data
+        $request->validate([
+            'merchantRefNum' => 'required|string',
+        ]);
+
+        // Extract the reference number
+        $merchantRefNum = $request->merchantRefNum;
+        // Start Confirmation Order
+        // Start Confirmation Order
+        // Get payment status
+        $response = $this->fawryPayService->getPaymentStatus($merchantRefNum);
+
+        if(isset($response['orderStatus']) && $response['orderStatus'] == 'EXPIRED' or $response['orderStatus'] == 'UNPAID'){
+            return response()->json([
+                'faild'=>'Something Wrong',
+            ],498);
+        };
+       
+          return $orderConfirmation = $this->confirmOrder($response);
+            if($orderConfirmation){
+                return response()->json([
+                    'success'=>'Order Success',
+                    'orderDetales'=>[
+                        'paymentAmount'=>$response['paymentAmount'],
+                        'paymentMethod'=>$response['paymentMethod'],
+                    ]
+                ]);
+            }
+
+    // Return response to the client
+    // return response()->json($response,200);
+    }
+
 }
